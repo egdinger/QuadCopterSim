dc7ed23a29e1c5c8c9eb5849fa5017cf98747880
Eric Dinger <egdinger@gmail.com>
2012-04-10 17:46:48 -0700

    Made new classes for state and blank classes for the the new sensors

--- sim.py
+++ sim.py
@@ -12,6 +12,19 @@ class ThreeState:
                 self.y = 0.0
                 self.z = 0.0
 
+class physical_state:
+        def __init__(self):
+                #These are the absoulte positions
+                self.position = ThreeState()
+                self.velocity = ThreeState()
+                self.acceleration = ThreeState()
+                #these are the rotations
+                self.angle = ThreeState()
+                self.angle_velocity = ThreeState()
+                self.angle_accl = ThreeState()
+                self.prop_speed = [0,0,0,0] #This is in rads/sec
+        
+
 class quad_state:
         def __init__(self):
                 self.roll = 0.0
@@ -23,7 +36,7 @@ class quad_state:
 
 #this is the class that models the acceleromter
 #it will model the adxl345
-class accel:
+class accel_model:
         #0-1023 is the range of value
         #2g scale
         #noise on z axis =< 1.5lsb
@@ -62,6 +75,37 @@ class accel:
         def scale(self):
                 return self.fullscale
 
+#xyz should be changed to psi, phi, theta
+class gyro_model:
+        def __init__(self):
+                self.current = ThreeState()
+                self.previous = ThreeState()
+
+        def measure(self):
+                return self.current
+        
+#xyz should be changed to psi, phi, theta, I think, look into convetions
+class mag_model():
+        def __init__(self):
+                self.heading = ThreeState()
+
+        def measure(self):
+                return self.heading
+
+class distance_model():
+        def __init__(self):
+                self.reading = 0
+
+        def measure(self):
+                return self.reading
+
+class imu_model:
+        def __init__(self):
+                self.accel = accel_model()
+                self.gyro = gyro_model()
+                self.mag = mag_model()
+                self.alt = distance_model()
+
 class QuadCopter:
 	#units are grams
 	mass = 1000
@@ -85,7 +129,6 @@ class QuadCopter:
 		for p in motor_percent:
 			z_thrust += p*self.max_motor_speed*self.thrust_rpm
 
-                #Need to check if this the correct math, I'm not sure it is
 		x_thrust += (motor_percent[0]*self.max_motor_speed*self.thrust_rpm)*math.sin(self.phys_state.roll)
 		x_thrust += (motor_percent[2]*self.max_motor_speed*self.thrust_rpm)*math.sin(self.phys_state.roll)
 		y_thrust += (motor_percent[1]*self.max_motor_speed*self.thrust_rpm)*math.sin(self.phys_state.pitch)
@@ -96,21 +139,21 @@ class QuadCopter:
 
 x = [.1,.2,.3,.4,.5,.6,.7,.8,.9,1]
 
-for p in x:
-        z = [p,p,p,p]
-        print (QuadCopter.thrust(z, QuadCopter))
-
-QuadCopter.phys_state.roll = .1
-print('------------------------------------------------')
-print(QuadCopter.phys_state.roll)
-for p in x:
-        z = [p,p,p,p]
-        print (QuadCopter.thrust(z, QuadCopter))
-
-print('------------------------------------------------')
-a= accel()
-QuadCopter.phys_state.roll = 0
-for i in range(20):
-        f = a.measure(QuadCopter.phys_state)
-        print (math.sqrt((f.x-512)*(f.x-512)+(f.z-512)*(f.z-512)))
-        print(f.x, f.y, f.z)
+##for p in x:
+##        z = [p,p,p,p]
+##        print (QuadCopter.thrust(z, QuadCopter))
+##
+##QuadCopter.phys_state.roll = .1
+##print('------------------------------------------------')
+##print(QuadCopter.phys_state.roll)
+##for p in x:
+##        z = [p,p,p,p]
+##        print (QuadCopter.thrust(z, QuadCopter))
+##
+##print('------------------------------------------------')
+##a= accel()
+##QuadCopter.phys_state.roll = 0
+##for i in range(20):
+##        f = a.measure(QuadCopter.phys_state)
+##        print (math.sqrt((f.x-512)*(f.x-512)+(f.z-512)*(f.z-512)))
+##        print(f.x, f.y, f.z)
